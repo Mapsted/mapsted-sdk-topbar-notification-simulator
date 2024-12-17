65fa8eb3c414cc06c35eeea6d4baf069566e137b
Vimal Bosamiya <vimal.bosamiya@mapsted.com>
2024-12-17 05:37:21 -0800

    Update to SDK v6.1.0

---
+++ MapstedTopBarNotification.framework/Headers/MapstedTopBarNotification.h
@@ -0,0 +1,18 @@
+//
+//  MapstedNotification.h
+//  MapstedNotification
+//
+//  Created by Daniel on 2021-09-29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapstedTriggersCore.
+FOUNDATION_EXPORT double MapstedNotificationVersionNumber;
+
+//! Project version string for MapstedTriggersCore.
+FOUNDATION_EXPORT const unsigned char MapstedNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapstedTriggersCore/PublicHeader.h>
+
+
